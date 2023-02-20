060849805c2f53caebda8b9ce3b9cb2d7e26d95e
BZ <72524168+BeniZan@users.noreply.github.com>
2023-02-20 22:54:37 +0200

    Add files via upload

---
+++ Utils/FileSystem/Loader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using System.Reflection; 
+public class Loader
+{ 
+    public static T Load<T>(string path) where T : UnityEngine.Object
+    {
+      T item = Resources.Load<T>(path);
+        if ( ! item )
+            Debug.LogWarning($"Couldn't find resource type of { typeof(T) } at {path}"  ); 
+
+        return item;
+    }    
+    public static T[] LoadAll<T>(string path) where T : UnityEngine.Object
+    {
+       T[] items = Resources.LoadAll<T>(path);
+       if (items.Length == 0 || items == null)
+            Debug.LogWarning($"Couldn't find resources of type <({typeof(T)})>[]  at {path}"); 
+         
+        return items; 
+    }
+
+    public static Object LoadGeneric(string path , System.Type type)
+    {
+        MethodInfo methodInfo = typeof(Loader).GetMethod(
+            nameof(Load) , BindingFlags.Public | BindingFlags.Static); 
+
+        methodInfo = methodInfo.MakeGenericMethod(type); 
+
+        return methodInfo.Invoke(null, new object[] { path } ) as Object; 
+    }
+
+    public static Object[] LoadAllGeneric(string path, System.Type type)
+    {
+        MethodInfo methodInfo = typeof(Loader).GetMethod(
+            nameof(LoadAll), BindingFlags.Public | BindingFlags.Static);
+         
+        methodInfo = methodInfo.MakeGenericMethod(type.GetElementType());
+
+        return methodInfo.Invoke(null, new object[] { path }) as Object[];
+    }
+
+}
